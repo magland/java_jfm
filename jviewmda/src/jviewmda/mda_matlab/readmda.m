a0ff8c9bc1140291fef3b9627e34470495295fc9
Jeremy Magland <jeremy.magland@gmail.com>
2015-02-06 15:34:13 -0500

    mda testing stuff

--- jviewmda/src/jviewmda/mda_matlab/readmda.m
+++ jviewmda/src/jviewmda/mda_matlab/readmda.m
@@ -20,16 +20,18 @@ N=prod(S);
 A=zeros(S);
 if (code==-1)
     M=zeros(1,N*2);
-    M(:)=fread(F,N*2,'float');
+    M(:)=fread(F,N*2,'float32');
     A(:)=M(1:2:prod(S)*2)+i*M(2:2:prod(S)*2); %Split into real/imag
 elseif (code==-2)
     A(:)=fread(F,N,'uchar');
 elseif (code==-3)
-    A(:)=fread(F,N,'float');
+    A(:)=fread(F,N,'float32');
 elseif (code==-4)
-    A(:)=fread(F,N,'short');
+    A(:)=fread(F,N,'int16');
 elseif (code==-5)
-    A(:)=fread(F,N,'int');
+    A(:)=fread(F,N,'int32');
+elseif (code==-6)
+    A(:)=fread(F,N,'uint16');
 end;
 
 fclose(F);
